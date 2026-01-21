c2ad3ad57d91ed58db3ba6f0304b59f9c201c251
Jimmy Pan <jimmypan13570@gmail.com>
2026-01-21 00:55:14 -0700

    update check for updates method

--- LinuxMCBot/updates.py
+++ LinuxMCBot/updates.py
@@ -1,24 +1,23 @@
 import utils
 import config
 
-from urllib.request import urlopen, urlretrieve
+import requests
 from discord.ext import tasks, commands
 
 
 def check():
-  url = "https://raw.githubusercontent.com/Nexumi/LinuxMCBot/main/LinuxMCBot/config.py"
-  try:
-    version = eval("\n".join([line.decode("utf-8") for line in urlopen(url).read().splitlines()[-5:]]).replace("version = ", ""))
-  except:
-    return None
+  url = "https://api.github.com/repos/Nexumi/LinuxMCBot/releases/latest"
+  response = requests.get(url)
+  data = response.json()
+  version = data.get("tag_name")
 
   if new(version):
     print("[\033[34mnotice\033[0m] A new release of LinuxMCBot is available: \033[31m" +\
-      simpleName() + "\033[0m -> \033[32m" + simpleName(version) + "\033[0m")
+      simpleName() + "\033[0m -> \033[32m" + version + "\033[0m")
 
 
 def new(version):
-  return tuple(config.version.values()) < tuple(version.values())
+  return tuple(config.version.values()) < tuple(map(int, version[1:].split(".")))
 
 def simpleName(v = config.version):
   return f"v{v['major']}.{v['minor']}.{v['patch']}"
